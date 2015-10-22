31a7504d5495123da8464627682c98cfb503e1e3
Alexis Mousset <contact@amousset.me>
2015-10-22 23:34:04 +0200

    Add becnhmarks

---
+++ benches/smtp.rs
@@ -0,0 +1,45 @@
+#![feature(test)]
+
+extern crate lettre;
+extern crate test;
+
+use lettre::transport::smtp::SmtpTransportBuilder;
+use lettre::transport::EmailTransport;
+use lettre::mailer::Mailer;
+use lettre::email::EmailBuilder;
+
+#[bench]
+fn bench_simple_send(b: &mut test::Bencher) {
+    b.iter(|| {
+        let sender = SmtpTransportBuilder::localhost().unwrap().build();
+        let mut mailer = Mailer::new(sender);
+        let email = EmailBuilder::new()
+                        .to("root@localhost")
+                        .from("user@localhost")
+                        .body("Hello World!")
+                        .subject("Hello")
+                        .build()
+                        .unwrap();
+        let result = mailer.send(email);
+        assert!(result.is_ok());
+        mailer.close()
+    });
+}
+
+#[bench]
+fn bench_reuse_send(b: &mut test::Bencher) {
+    let sender = SmtpTransportBuilder::localhost().unwrap().connection_reuse(true).build();
+    let mut mailer = Mailer::new(sender);
+    b.iter(|| {
+        let email = EmailBuilder::new()
+                        .to("root@localhost")
+                        .from("user@localhost")
+                        .body("Hello World!")
+                        .subject("Hello")
+                        .build()
+                        .unwrap();
+        let result = mailer.send(email);
+        assert!(result.is_ok());
+    });
+    mailer.close()
+}
