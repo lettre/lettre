401118ee688980e804ab53cdfb6cf39d6553a3d9
Alexis Mousset <contact@amousset.me>
2015-10-26 00:17:46 +0100

    Improved test for File transport

--- tests/transport_file.rs
+++ tests/transport_file.rs
@@ -1,10 +1,13 @@
 extern crate lettre;
 
 use std::env::temp_dir;
+use std::fs::File;
+use std::fs::remove_file;
+use std::io::Read;
 
 use lettre::transport::file::FileEmailTransport;
 use lettre::transport::EmailTransport;
-use lettre::email::EmailBuilder;
+use lettre::email::{SendableEmail, EmailBuilder};
 
 #[test]
 fn file_transport() {
@@ -16,9 +19,16 @@ fn file_transport() {
                     .subject("Hello")
                     .build()
                     .unwrap();
-    let result = sender.send(email);
+    let result = sender.send(email.clone());
     assert!(result.is_ok());
 
-    message_id = email.message_id();
+    let message_id = email.message_id();
+    let file = format!("{}/{}.txt", temp_dir().to_str().unwrap(), message_id);
+    let mut f = File::open(file.clone()).unwrap();
+    let mut buffer = String::new();
+    let _ = f.read_to_string(&mut buffer);
 
+    assert_eq!(buffer, format!("{}: from=<user@localhost> to=<root@localhost>\n{}", message_id, email.message()));
+
+    remove_file(file).unwrap();
 }
