3612ffca7a41522e81b6e860b8608d052571db02
Alexis Mousset <contact@amousset.me>
2015-10-30 19:08:11 +0100

    Fix becnhmarks

--- benches/transport_smtp.rs
+++ benches/transport_smtp.rs
@@ -5,13 +5,11 @@ extern crate test;
 
 use lettre::transport::smtp::SmtpTransportBuilder;
 use lettre::transport::EmailTransport;
-use lettre::mailer::Mailer;
 use lettre::email::EmailBuilder;
 
 #[bench]
 fn bench_simple_send(b: &mut test::Bencher) {
-    let sender = SmtpTransportBuilder::new("127.0.0.1:2525").unwrap().build();
-    let mut mailer = Mailer::new(sender);
+    let mut sender = SmtpTransportBuilder::new("127.0.0.1:2525").unwrap().build();
     b.iter(|| {
         let email = EmailBuilder::new()
                         .to("root@localhost")
@@ -20,18 +18,17 @@ fn bench_simple_send(b: &mut test::Bencher) {
                         .subject("Hello")
                         .build()
                         .unwrap();
-        let result = mailer.send(email);
+        let result = sender.send(email);
         assert!(result.is_ok());
     });
 }
 
 #[bench]
 fn bench_reuse_send(b: &mut test::Bencher) {
-    let sender = SmtpTransportBuilder::new("127.0.0.1:2525")
+    let mut sender = SmtpTransportBuilder::new("127.0.0.1:2525")
                      .unwrap()
                      .connection_reuse(true)
                      .build();
-    let mut mailer = Mailer::new(sender);
     b.iter(|| {
         let email = EmailBuilder::new()
                         .to("root@localhost")
@@ -40,8 +37,8 @@ fn bench_reuse_send(b: &mut test::Bencher) {
                         .subject("Hello")
                         .build()
                         .unwrap();
-        let result = mailer.send(email);
+        let result = sender.send(email);
         assert!(result.is_ok());
     });
-    mailer.close()
+    sender.close()
 }
