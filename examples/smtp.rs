73c957e3504534c4012b47742ddc68a46aa41384
Alexis Mousset <contact@amousset.me>
2016-10-23 12:37:30 +0200

    docs(all): Add an example for simple builder usage and smtp transport

---
+++ examples/smtp.rs
@@ -0,0 +1,30 @@
+extern crate lettre;
+
+use lettre::email::EmailBuilder;
+use lettre::transport::EmailTransport;
+use lettre::transport::smtp::SmtpTransportBuilder;
+
+fn main() {
+    let email = EmailBuilder::new()
+        // Addresses can be specified by the tuple (email, alias)
+        .to(("user@example.org", "Firstname Lastname"))
+        // ... or by an address only
+        .from("user@example.com")
+        .subject("Hi, Hello world")
+        .text("Hello world.")
+        .build()
+        .unwrap();
+
+    // Open a local connection on port 25
+    let mut mailer = SmtpTransportBuilder::localhost().unwrap().build();
+    // Send the email
+    let result = mailer.send(email);
+
+    if result.is_ok() {
+        println!("Email sent");
+    } else {
+        println!("Could not send email: {:?}", result);
+    }
+
+    assert!(result.is_ok());
+}
