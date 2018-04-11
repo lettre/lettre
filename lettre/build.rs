81bad1317519d330c46ea02f2b7a266b97cc00dd
Bastien Orivel <eijebong@bananium.fr>
2018-04-11 17:46:00 +0200

    test(lettre): Replace skeptic by some custom rustdoc invocations
    Unfortunately skeptic pulls all its dependencies in projects using
    lettre (see https://github.com/budziq/rust-skeptic/issues/60).

--- lettre/build.rs
+++
@@ -1,8 +0,0 @@
-extern crate skeptic;
-
-use skeptic::*;
-
-fn main() {
-    let mdbook_files = markdown_files_of_directory("../website/content/sending-messages/");
-    generate_doc_tests(&mdbook_files);
-}
