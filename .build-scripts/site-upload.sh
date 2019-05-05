574afb0c9b34f909d2faa872c90a666415b9c5c9
Alexis Mousset <alexis.mousset@rudder.io>
2019-05-05 20:47:09 +0200

    fix(all): Properly override favicon in docs theme

--- .build-scripts/site-upload.sh
+++ .build-scripts/site-upload.sh
@@ -4,7 +4,6 @@ set -xe
 
 cd website
 make clean && make
-cp static/images/favicon.png _book/html/favicon.png
 echo "lettre.at" > _book/html/CNAME
 sudo pip install ghp-import
 ghp-import -n _book/html
