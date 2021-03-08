6667bc68688b62c79d1b37915de2b62b55861644
Paolo Barbolini <paolo@paolo565.org>
2021-03-08 11:18:34 +0100

    Test using all supported async executors

--- tests/transport_stub.rs
+++ tests/transport_stub.rs
@@ -1,15 +1,10 @@
 #[cfg(test)]
 #[cfg(feature = "builder")]
-mod test {
-    use lettre::{transport::stub::StubTransport, Message};
-
-    #[cfg(feature = "tokio02")]
-    use tokio02_crate as tokio;
+mod sync {
+    use lettre::{transport::stub::StubTransport, Message, Transport};
 
     #[test]
     fn stub_transport() {
-        use lettre::Transport;
-
         let sender_ok = StubTransport::new_ok();
         let sender_ko = StubTransport::new_error();
         let email = Message::builder()
@@ -23,12 +18,17 @@ mod test {
         sender_ok.send(&email).unwrap();
         sender_ko.send(&email).unwrap_err();
     }
+}
 
-    #[cfg(feature = "async-std1")]
-    #[async_std::test]
-    async fn stub_transport_asyncstd1() {
-        use lettre::AsyncTransport;
+#[cfg(test)]
+#[cfg(all(feature = "builder", feature = "tokio02"))]
+mod tokio_02 {
+    use lettre::{transport::stub::StubTransport, AsyncTransport, Message};
 
+    use tokio02_crate as tokio;
+
+    #[tokio::test]
+    async fn stub_transport_tokio02() {
         let sender_ok = StubTransport::new_ok();
         let sender_ko = StubTransport::new_error();
         let email = Message::builder()
@@ -43,12 +43,40 @@ mod test {
         sender_ok.send(email.clone()).await.unwrap();
         sender_ko.send(email).await.unwrap_err();
     }
+}
 
-    #[cfg(feature = "tokio02")]
+#[cfg(test)]
+#[cfg(all(feature = "builder", feature = "tokio1"))]
+mod tokio_1 {
+    use lettre::{transport::stub::StubTransport, AsyncTransport, Message};
+
+    use tokio1_crate as tokio;
+
     #[tokio::test]
-    async fn stub_transport_tokio02() {
-        use lettre::AsyncTransport;
+    async fn stub_transport_tokio1() {
+        let sender_ok = StubTransport::new_ok();
+        let sender_ko = StubTransport::new_error();
+        let email = Message::builder()
+            .from("NoBody <nobody@domain.tld>".parse().unwrap())
+            .reply_to("Yuin <yuin@domain.tld>".parse().unwrap())
+            .to("Hei <hei@domain.tld>".parse().unwrap())
+            .subject("Happy new year")
+            .date("Tue, 15 Nov 1994 08:12:31 GMT".parse().unwrap())
+            .body(String::from("Be happy!"))
+            .unwrap();
 
+        sender_ok.send(email.clone()).await.unwrap();
+        sender_ko.send(email).await.unwrap_err();
+    }
+}
+
+#[cfg(test)]
+#[cfg(all(feature = "builder", feature = "async-std1"))]
+mod asyncstd_1 {
+    use lettre::{transport::stub::StubTransport, AsyncTransport, Message};
+
+    #[async_std::test]
+    async fn stub_transport_asyncstd1() {
         let sender_ok = StubTransport::new_ok();
         let sender_ko = StubTransport::new_error();
         let email = Message::builder()
