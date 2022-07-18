75749062f09724b9db3e1bf8be244328ce3b67aa
Paolo Barbolini <paolo.barbolini@m4ss.net>
2022-07-18 09:33:09 +0200

    'best option' -> 'most secure option'

--- src/transport/smtp/client/tls.rs
+++ src/transport/smtp/client/tls.rs
@@ -44,9 +44,9 @@ pub enum TlsVersion {
     Tlsv12,
     /// TLS 1.3
     ///
-    /// The best option, altough not supported by all SMTP servers.
+    /// The most secure option, altough not supported by all SMTP servers.
     ///
-    /// Altough it is technically supported by all TLS backends
+    /// Altough it is technically supported by all TLS backends,
     /// trying to set it for `native-tls` will give a runtime error.
     Tlsv13,
 }
