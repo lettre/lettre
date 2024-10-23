51794aa9121ced5e34fb34f986d5ff0bd731071a
Paolo Barbolini <paolo.barbolini@m4ss.net>
2024-10-23 23:04:45 +0200

    Prepare v0.11.10 (#1002)

--- src/lib.rs
+++ src/lib.rs
@@ -109,7 +109,7 @@
 //! [mime 0.3]: https://docs.rs/mime/0.3
 //! [DKIM]: https://datatracker.ietf.org/doc/html/rfc6376
 
-#![doc(html_root_url = "https://docs.rs/crate/lettre/0.11.9")]
+#![doc(html_root_url = "https://docs.rs/crate/lettre/0.11.10")]
 #![doc(html_favicon_url = "https://lettre.rs/favicon.ico")]
 #![doc(html_logo_url = "https://avatars0.githubusercontent.com/u/15113230?v=4")]
 #![forbid(unsafe_code)]
