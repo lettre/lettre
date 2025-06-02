716d7baac284415bd19db0a70f4c16e6958c27a8
Paolo Barbolini <paolo.barbolini@m4ss.net>
2025-06-02 11:40:45 +0200

    regression

--- src/transport/smtp/client/mod.rs
+++ src/transport/smtp/client/mod.rs
@@ -39,7 +39,9 @@ pub(super) use self::tls::current::InnerTlsParameters;
 pub use self::tls::current::TlsVersion;
 pub use self::{
     connection::SmtpConnection,
-    tls::current::{Certificate, Identity, Tls, TlsParameters, TlsParametersBuilder},
+    tls::current::{
+        Certificate, CertificateStore, Identity, Tls, TlsParameters, TlsParametersBuilder,
+    },
 };
 
 #[cfg(any(feature = "tokio1", feature = "async-std1"))]
